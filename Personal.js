9fa9a6768fe6b5e8e861f4e1b3fadafd5bab4f45
Abilas <abilas96@gmail.com>
2016-09-03 23:04:01 -0400

    Adding pages

--- Personal.js
+++ Personal.js
@@ -1,1 +1,33 @@
-jQuery( document ).ready(function() {
+$( document ).ready(function() {
+  var elementPosition = $('#nav').offset();
+  
+  $(window).scroll(function(){
+    if($(window).scrollTop() > elementPosition.top){
+      $('#nav').css('position','fixed').css('top','0');
+      $("#back-top").fadeIn();
+    } else {
+      $('#nav').css('position','static');
+      $("#back-top").fadeOut();
+    }    
+  });
+  
+  $("#back-top").click(function() {
+    $('html,body').animate({
+        scrollTop: $("body").offset().top},
+        'slow');
+  });
+  
+  $('#aboutBtn').click(function() {
+    console.log("working");
+    $('html,body').animate({
+        scrollTop: $("#aboutMe").offset().top},
+        'slow');
+  });
+  
+  $('#linksBtn').click(function() {
+    console.log("working");
+    $('html,body').animate({
+        scrollTop: $("#links").offset().top},
+        'slow');
+  });
+});
