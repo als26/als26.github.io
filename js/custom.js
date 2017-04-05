f04529b92111e76c25aef14adf0250da58005a8d
Abilas <abilas96@gmail.com>
2017-04-04 22:47:18 -0400

    added projects page

--- js/custom.js
+++ js/custom.js
@@ -1,29 +1,53 @@
 $( document ).ready(function() {
   //$("#back-top-div").fadeIn("slow");
   //var elementPosition = $('#nav').offset();
-  
-  $("#back-top-div").click(function(){
-     $('html,body').animate({
-        scrollTop: $("#aboutPage").offset().top},
-        'slow');
-  });
-  
+  var projectIndex = 1;
+  showDivs(projectIndex);
+
+
   //Clicking functionality for links
   $('#resume').click(function(){
     window.open('https://drive.google.com/open?id=0B8qsA4dGzxs0RXU3NkJXZjZPMDA', '_blank');
   });
-  
+
   $('#linkedin').click(function(){
     window.open('https://www.linkedin.com/in/abilassathiyanesan', '_blank');
   });
-  
+
   $('#github').click(function(){
     window.open('https://github.com/als26', '_blank');
   });
-  
+
   $('#contact').click(function(){
     window.location.href = "mailto:a6sathiy@uwaterloo.ca?";
   });
-  
-  
+
+  $('.display-left').on('click', function() {
+    console.log("left");
+    showDivs(projectIndex -= 1);
+  });
+
+  $('.display-right').on('click', function() {
+    console.log("right");
+    showDivs(projectIndex += 1);
+  });
+  // function leftScroll() {
+  //   showDivs(projectIndex -= 1);
+  // }
+
+  // function rightScroll() {
+  //   showDivs(projectIndex += 1);
+  // }
+
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("myProjects");
+    if (n > x.length) {projectIndex = 1}
+    if (n < 1) {projectIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    x[projectIndex-1].style.display = "block";
+  }
+
 });
